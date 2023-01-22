06fc41dd4ab598e7838e9ce4bc8d9fe74b10569b
King2oo9 <120119639+King2oo9@users.noreply.github.com>
2023-01-21 22:30:19 -0800

    added template

--- uzb/contactus/views.py
+++ uzb/contactus/views.py
@@ -6,11 +6,16 @@ from .models import Contact
 # Create your views here.
 def asosiy(request):
     if request.method == 'POST':
-        form = Cnt(request.POST)
-        if form.is_valid():
-            form.save()
-            print('Saqlandi')
-            return redirect('contact')
-        else:
-            print('xatolik bor')
+        ism = request.POST.get('ism')
+        mavzu = request.POST.get('mavzu')
+        matn = request.POST.get('matn')
+        contatc=Contact(ism=ism, mavzu=mavzu, matn=matn)
+        contatc.save()
+        # form = Cnt(request.POST)
+        # if form.is_valid():
+        #     form.save()
+        #     print('Saqlandi')
+        #     return redirect('contact')
+        # else:
+        #     print('xatolik bor')
     return render(request, 'contactus/index.html')
